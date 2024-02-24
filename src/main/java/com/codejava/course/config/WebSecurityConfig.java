f4129f78df64bf26ec36c7f3757222eb9f73e0bd
trankimhoang00246 <trankimhoang1710@gmail.com>
2024-02-24 15:10:19 +0700

    feat: remove web security config in .gitignore

---
+++ src/main/java/com/codejava/course/config/WebSecurityConfig.java
@@ -0,0 +1,65 @@
+package com.codejava.course.config;
+
+import com.codejava.course.security.JwtAuthFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+@EnableMethodSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain getSecurityFilterChain(
+            HttpSecurity http,
+            JwtAuthFilter jwtAuthFilter) throws Exception {
+        return http
+                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+                .httpBasic(httpSecurityHttpBasicConfigurer -> httpSecurityHttpBasicConfigurer.disable())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((authentication) ->
+                        authentication
+                                .requestMatchers("/api/v1/auth/register", "/api/v1/auth/login", "/error").anonymous()
+                                .requestMatchers("/v3/api-docs/**").permitAll()
+                                .requestMatchers("/swagger-ui/**").permitAll()
+                                .requestMatchers("/swagger-resources/**").permitAll()
+                                .requestMatchers("/v3/api-ui.html").permitAll()
+                                .requestMatchers("/webjars/**").permitAll()
+                                .requestMatchers("/swagger-ui.html").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .cors(cors -> cors
+                        .configurationSource(request -> {
+                            CorsConfiguration cfg = new CorsConfiguration();
+                            cfg.setAllowedOrigins(Collections.singletonList("*"));
+                            cfg.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                            cfg.setAllowedHeaders(Arrays.asList("*"));
+                            return cfg;
+                        })
+                )
+                .build();
+    }
+}
---
+++ src/main/java/com/codejava/course/controller/CollabRequestController.java
@@ -0,0 +1,2 @@
+package com.codejava.course.controller;public class CollabRequestController {
+}
